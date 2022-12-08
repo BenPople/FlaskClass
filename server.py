5322586a0ab8e0e338f8a0f0d5cd3e9efd18a9b0
Ben Pople <benpople@outlook.com>
2022-12-08 15:14:02 +0000

    Added sending messages to the SenseHat LEDs!

--- server.py
+++ server.py
@@ -94,5 +94,56 @@ def sensors():
 
     return response
 
+@app.route('/api/v1/LED/SendLEDMessage', methods=['GET']) 
+def sendMessage():
+    message = request.args.get('message')
+    
+    try:
+        if 'backColour' not in request.args:
+            trueBackColour = [0,0,0]
+        else:
+            acceptedColours = ['Red', 'Green', 'Blue']
+            backColour = request.args.get('backColour')
+            
+            if backColour in acceptedColours:
+                if backColour == 'Red':
+                    trueBackColour = [255,0,0]
+                elif backColour == 'Green':
+                    trueBackColour = [0,255,0]
+                else:
+                    trueBackColour = [0,0,255]
+            else:   
+                backColour = request.args.get('backColour').split(',')
+                trueBackColour = []
+
+                for value in backColour:
+                    trueBackColour.append(int(value))
+
+        Sense.show_message(message, back_colour=trueBackColour)
+        
+        successResponseJSON = jsonify(
+            {
+                "SetMessageSuccess" : True,
+                "SetMessage" : message
+                }
+            )
+
+        successResponseJSON.headers.add('Access-Control-Allow-Origin', '*')
+            
+        return successResponseJSON
+    except:
+            Sense.clear()
+            
+            successResponseJSON = jsonify(
+                {
+                    "SetMessageSuccess" : False,
+                    "SetMessage" : "ERROR"
+                    }
+                )
+
+            successResponseJSON.headers.add('Access-Control-Allow-Origin', '*')
+                
+            return successResponseJSON
+
 if __name__ == '__main__':
     app.run(debug=True)
